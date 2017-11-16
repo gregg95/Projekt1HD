f1c17808327a8977b7cbca57f0179631be300eb7
Grzegorz Giera <grzegorzgiera95@gmail.com>
2017-11-16 15:56:34 +0100

    Initialize

---
+++ Projekt1HD/Models/CommentReply.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1HD.Models
+{
+    public class CommentReply
+    {
+        public string Respondent { get; set; }
+        public string Content { get; set; }
+    }
+}
---
+++ Projekt1HD/Models/Database_Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt1HD.Models
+{
+    public class Database_Item
+    {
+        public int Item_ID { get; set; }
+        public string Item_type { get; set; }
+        public string Item_brand { get; set; }
+        public string Item_model { get; set; }
+        public string Additional_comments { get; set; }
+
+    }
+}
